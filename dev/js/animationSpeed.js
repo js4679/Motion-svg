0ad575a7ea358e88831027bb4edfc966c3fa3d28
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-19 10:03:39 -0400

    clear out

--- dev/js/animationSpeed.js
+++
@@ -1,1 +0,0 @@
-export const boxSpeed = 2;
--- dev/js/greenBox.js
+++
@@ -1,9 +0,0 @@
-import {gsap} from "gsap";
-
-
-export function greenBoxAnimation(){
-    var tl = gsap.timeline();
-    tl.from(".green-box",{duration: 2, y: 300})
-        .to(".green-box",{duration: 2, x: 300});
-    return tl;
-}
