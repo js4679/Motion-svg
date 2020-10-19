9b4a9d484326225b3e7051b39f2f9498a1004838
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-19 11:57:00 -0400

    before restart

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,8 +1,5 @@
 // import {gsap} from "gsap";
 
-// gsap.to(".red-box",{duration: 2, alpha:0});
-
-// gsap.to(".blue-box",{duration: 4, alpha:0});
 
 
 
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,19 +1,7 @@
 // import * as Demo from './demo.js';
 
 import {gsap} from "gsap";
-import {redBoxAnimation} from './redBox.js';
-import {blueBoxAnimation} from './blueBox.js';
-import {greenBoxAnimation} from './greenBox.js';
+
 
 // console.log(Demo);
 
-var mainTL = gsap.timeline({paused:true});
-
-mainTL.add(redBoxAnimation())
-        //.addLabel("blue")
-        // .addPause()
-        .add(blueBoxAnimation())
-       
-        .add(greenBoxAnimation())
-        // .seek("blue")
-        .play();
