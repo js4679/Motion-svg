64597e3cd41775f41898395cf66bd838889ec0ae
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-20 18:41:27 -0400

    complete

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -6,17 +6,17 @@ const bubbleSpeed=1.5;
 
 const bubbleTL =gsap.timeline();
 bubbleTL.from("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -170, ease: "none"}, "bubblego")
-        .to("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 170, ease: "none"})
+        .to("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 240, ease: "none"})
         .to("#sliderColor-",{ width: 100})
         .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -130, ease: "none"}, "bubblego")
         .to("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"}, "away1")
         .to("#sliderColor-",{ width: 150})
         .from("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -100, ease: "none"}, "bubblego")
-        .to("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 100, ease: "none"}, "away2")
+        .to("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 170, ease: "none"}, "away2")
         .to("#sliderColor-",{ width: 224})
         .from("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -120, ease: "none"}, "bubblego")
-        .to("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"}, "away3")
-        .to("#sliderColor-",{ duration: 1.5, scaleX: 30, transformOrigin: "center" })
+        .to("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 190, ease: "none"}, "away3")
+        .to("#sliderColor-",{ duration: 1, scaleX: 30, transformOrigin: "center" })
         .to("#sliderColor-",{ duration: 1, scale: 100, transformOrigin: "center" });
 
 
