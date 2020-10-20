b709f9c461f6a8f40df66d22ac36b8d771a44701
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-20 12:32:49 -0400

    staggerd slider

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -8,7 +8,7 @@ const bubbleTL =gsap.timeline();
 bubbleTL.from("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -170, ease: "none"})
         .to("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 170, ease: "none"})
         .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -130, ease: "none"})
-        .to("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 40, ease: "none"})
+        .to("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"})
         .from("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -100, ease: "none"})
         .to("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 100, ease: "none"})
         .from("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -120, ease: "none"})
@@ -22,7 +22,11 @@ export function bubbleAnimation(){
 // gsap.set("#sliderColor-",{transformOrigin: "center"});
 
 const loaderTL =gsap.timeline();
-loaderTL.from("#sliderColor-", {duration:bubbleSpeed,  x: -70, ease: "none"})
+loaderTL.from("#sliderColor-",{ width: 0})
+        .to("#sliderColor-",{ width: 50})
+        .to("#sliderColor-",{ width: 100})
+        .to("#sliderColor-",{ width: 158})
+        .to("#sliderColor-",{ width: 300});
 
 export function loaderAnimation(){
     return loaderTL;
