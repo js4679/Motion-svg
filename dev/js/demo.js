bbd862d0bdc442a058738c038171f60d2b4284d1
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-19 22:35:53 -0400

    loader

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,12 +1,12 @@
 import {gsap} from "gsap";
 
-gsap.set("#smallestBubble",{transformOrigin: "top"});
+// gsap.set("#smallestBubble",{transformOrigin: "top"});
 
 const bubbleSpeed=1;
 
 const bubbleTL =gsap.timeline();
 bubbleTL.from("#smallestBubble", {duration:bubbleSpeed, alpha:0, y: -170, ease: "none"})
-        .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, y: -30, ease: "none"})
+        .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, y: -130, ease: "none"})
         .from("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, y: -100, ease: "none"})
         .from("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, y: -120, ease: "none"})
         .to("#smallestBubble", {duration:bubbleSpeed, alpha:0, y: 170, ease: "none"})
@@ -18,4 +18,9 @@ export function bubbleAnimation(){
     return bubbleTL;
 }
 
+const loaderTL =gsap.timeline();
+loaderTL.from("#sliderColor-", {duration:bubbleSpeed,  x: -70, ease: "none"})
 
+export function loaderAnimation(){
+    return loaderTL;
+}
