2c1931537d43d696eae371ec9bf68f286eb50c5f
js4679 <63320565+js4679@users.noreply.github.com>
2020-10-20 17:05:23 -0400

    complete except slider

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,19 +1,24 @@
 import {gsap} from "gsap";
 
-// gsap.set("#smallestBubble",{transformOrigin: "top"});
+gsap.set("#sliderColor-",{transformOrigin: "150"});
 
 const bubbleSpeed=1;
 
 const bubbleTL =gsap.timeline();
-bubbleTL.from("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -170, ease: "none"})
+bubbleTL.from("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -170, ease: "none"}, "bubblego")
         .to("#smallestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 170, ease: "none"})
-        .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -130, ease: "none"})
-        .to("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"})
-        .from("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -100, ease: "none"})
-        .to("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 100, ease: "none"})
-        .from("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -120, ease: "none"})
-        .to("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"});
-        // .to("#bubbles", {duration:bubbleSpeed, repeat:-1, ease: "none"});
+        .to("#sliderColor-",{ width: 50})
+        .from("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -130, ease: "none"}, "bubblego")
+        .to("#biggestBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"}, "away1")
+        .to("#sliderColor-",{ width: 100}, "away1")
+        .from("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -100, ease: "none"}, "bubblego")
+        .to("#mediumDarkBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 100, ease: "none"}, "away2")
+        .to("#sliderColor-",{ width: 158}, "away2")
+        .from("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: -120, ease: "none"}, "bubblego")
+        .to("#mediumLightBubble", {duration:bubbleSpeed, alpha:0, scale: 0, y: 120, ease: "none"}, "away3")
+        .to("#sliderColor-",{ width: 300}, "away3")
+        .to("#sliderColor-",{ scalex: 2}, "away3");
+
 
 export function bubbleAnimation(){
     return bubbleTL;
@@ -21,13 +26,13 @@ export function bubbleAnimation(){
 
 // gsap.set("#sliderColor-",{transformOrigin: "center"});
 
-const loaderTL =gsap.timeline();
-loaderTL.from("#sliderColor-",{ width: 0})
-        .to("#sliderColor-",{ width: 50})
-        .to("#sliderColor-",{ width: 100})
-        .to("#sliderColor-",{ width: 158})
-        .to("#sliderColor-",{ width: 300});
+// const loaderTL =gsap.timeline();
+// loaderTL.from("#sliderColor-",{ width: 0})
+//         .to("#sliderColor-",{ width: 50}, "bubblego")
+//         .to("#sliderColor-",{ width: 100}, "away1")
+//         .to("#sliderColor-",{ width: 158}, "away2")
+//         .to("#sliderColor-",{ width: 300}, "away3");
 
-export function loaderAnimation(){
-    return loaderTL;
-}
+// export function loaderAnimation(){
+//     return loaderTL;
+// }
